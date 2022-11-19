adaba9b2d0de9a08e855fb980a501876ba73bd4b
Yashaswi Anand <yash.kwr17@gmail.com>
2022-11-19 21:31:39 +0530

    project done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import MyCertificate from './components/MyCertificate';
+import MyJob from './components/MyJob';
+import MyOfferLetter from './components/MyOfferLetter';
+import MyPayment from './components/MyPayment';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={< Home />}></Route>
+        <Route exact path='/myjob' element={< MyJob />}></Route>
+        <Route exact path='/mypaymant' element={< MyPayment />}></Route>
+        <Route exact path='/myoffertter' element={< MyOfferLetter />}></Route>
+        <Route exact path='/mycertificate' element={< MyCertificate />}></Route>
+      </Routes>
     </div>
   );
 }
